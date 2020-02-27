3af18f1fd61e0333eaf767f742131971df720793
Daniela <drojas59@ucatolica.edu.co>
2020-02-27 10:56:24 -0500

    Daniela Rojas - Version 1

---
+++ apache.sh
@@ -0,0 +1,5 @@
+apt-get -y install apache2
+
+systemctl start apache2
+
+sudo ufw allow in "Apache Full"
---
+++ django.sh
@@ -0,0 +1,16 @@
+apt install python3
+
+apt install python3-pip
+
+pip3 install -U pip
+
+pip3 install virtualenv
+
+virtualenv ENV
+
+source djangoprojects/bin/activate
+
+sudo pip3 install django
+
+
+
